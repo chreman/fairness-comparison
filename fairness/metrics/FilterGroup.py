cb83f2a11bd8ae4ade86f04746aa409daba1c249
Christopher Kittel <web@christopherkittel.eu>
2018-08-20 23:47:00 +0200

    first implementation of subgroup metrics

---
+++ fairness/metrics/FilterGroup.py
@@ -0,0 +1,66 @@
+from fairness.metrics.Metric import Metric
+
+
+class FilterGroup(Metric):
+    def __init__(self, metric):
+        Metric.__init__(self)
+        self.metric = metric
+        self.name = metric.get_name()
+
+    def calc(self, actual, predicted, dict_of_sensitive_lists, single_sensitive_name,
+             unprotected_vals, positive_pred):
+
+        # print("unprotected_vals", unprotected_vals)
+        # print("sensitive_for_metric", self.sensitive_for_metric)
+        sensitive = dict_of_sensitive_lists[self.sensitive_for_metric]
+        sg_metrics = []
+        # print("protected_vals", set(sensitive))
+        for sens_expr in set(sensitive):
+            # print(sens_expr)
+            # make for loop here - for each sens_val do calc
+            # return max
+            # print("actual, sensitive")
+            # print(len(actual), len(sensitive))
+            # print(set(actual), set(sensitive))
+            sg_actual = [act for act, sens in zip(actual, sensitive)
+                         if sens in [sens_expr] + unprotected_vals]
+            # print("actual_sens")
+            # print(len(sg_actual))
+            # print(set(sg_actual))
+            sg_predicted = [pred for pred, sens in zip(predicted, sensitive)
+                            if sens in [sens_expr] + unprotected_vals]
+            # print("predicted_sens")
+            # print(len(sg_predicted))
+            # print(set(sg_predicted))
+            sg_sensitive = [sens for sens in sensitive
+                            if sens == sens_expr]
+            # print(len(sg_sensitive) / len(actual))
+
+            filtered_dict = {}
+            for sens_val in dict_of_sensitive_lists:
+                # print(sens_val)
+                other_sensitive = dict_of_sensitive_lists[sens_val]
+                # print("other sensitive")
+                # print(len(other_sensitive))
+                # print(set(other_sensitive))
+                filtered = [s for s, sens in zip(other_sensitive, sensitive)
+                            if sens in [sens_expr] + unprotected_vals]
+                # print("filtered")
+                # print(len(filtered))
+                # print(set(filtered))
+                filtered_dict[sens_val] = filtered
+            sg_metric = self.metric.calc(sg_actual, sg_predicted,
+                                         filtered_dict,
+                                         single_sensitive_name,
+                                         unprotected_vals, positive_pred)
+            sg_metrics.append(sg_metric)
+        return max(sg_metrics)
+
+    def set_subgroup_sensitive(self, sensitive_name):
+        """
+        Sets the sensitive attr to compare.
+
+        sensitive_name        sensitive attribute name (e.g., 'race')
+        """
+        self.name += str(sensitive_name)
+        self.sensitive_for_metric = sensitive_name
---
+++ fairness/metrics/SubgroupMetric.py
@@ -0,0 +1,44 @@
+from fairness.metrics.FilterGroup import FilterGroup
+from fairness.metrics.Metric import Metric
+
+
+class SubgroupMetric(Metric):
+    """
+    Takes the given metric and creates a version of it that calculates
+    metrics per subgroup.
+    It measures each protected subgroup against the unprotected group.
+
+    For example, for SubgroupMetric(CV), this measure calculates the CV score
+    for each comparison between the unprotected group and any protected subgroup,
+    and returns the maximum value as the final discrimination score.
+    """
+    def __init__(self, metric_class):
+        Metric.__init__(self)
+        self.metric = metric_class
+        self.name = self.metric().get_name()   # to be modified as this metric is expanded
+
+    def calc(self, actual, predicted, dict_of_sensitive_lists, single_sensitive_name,
+             unprotected_vals, positive_pred):
+        "modified to calculate metric first for each subgroup and then take max"
+        sgfilter = FilterGroup(self.metric())
+        sgfilter.set_subgroup_sensitive(self.sensitive_attr)
+        return sgfilter.calc(actual, predicted, dict_of_sensitive_lists, single_sensitive_name,
+                             unprotected_vals, positive_pred)
+
+    def expand_per_dataset(self, dataset, sensitive_dict, tag):
+        objects_list = []
+        for sensitive in dataset.get_sensitive_attributes_with_joint():
+            objects_list += self.make_subgroup_obj(sensitive)
+        return objects_list
+
+    def make_subgroup_obj(self, sensitive_attr):
+        obj = self.__class__(self.metric)
+        obj.set_subgroup_sensitive(sensitive_attr)
+        return obj
+
+    def set_subgroup_sensitive(self, sensitive_name):
+        """
+        Set the attribute and value to filter, i.e., to calculate this metric for.
+        """
+        self.sensitive_attr = sensitive_name
+        self.name = "Subgroups: " + str(sensitive_name) + "-" + self.name
